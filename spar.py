8355c14a5fd90262066685a49cfcf4ea2ff6a09c
Martin Schmidt <inw@immernurwollen.de>
2018-02-20 18:02:27 +0100

    trip data is stored in an array for further use

--- spar.py
+++ spar.py
@@ -2,45 +2,41 @@ from bahn.bahn import Sparbahn, TripData
 from datetime import datetime, timedelta, date
 
 
+
 def allweekdays(start_date, weekday, weeks):
-   d = start_date                  # January 1st
-   d += timedelta(days = weekday - d.weekday())  # First Sunday
+   """generates the date for a given weekday for x weeks in the future starting from start_date"""
+   d = start_date                  
+   d += timedelta(days = weekday - d.weekday()) 
    max_date = d + timedelta(days = 7*weeks)
    while d  <= max_date:
       if d >= datetime.now().date():
         yield d
       d += timedelta(days = 7)
 
-#weekdays 3 = Thu, 4=Fri, 5=Sat, 6=Sun
+
 
 start = 'Dresden Hbf'
 target = 'N'
-future_weeks = 4
+future_weeks = 1
 datesTo = list(allweekdays(datetime.now().date(),3,future_weeks))
-datesTo.extend(list(allweekdays(datetime.now().date(),4,future_weeks)))
+#datesTo.extend(list(allweekdays(datetime.now().date(),4,future_weeks)))
 datesBack = list(allweekdays(datetime.now().date(),5,future_weeks))
-datesBack.extend(list(allweekdays(datetime.now().date(),6,future_weeks)))
+#datesBack.extend(list(allweekdays(datetime.now().date(),6,future_weeks)))
 
+trips_to = []
+trips_back = []
 for i in range(0,future_weeks*2):
-    #print(datesTo[i])
-    #print(datesBack[i])
     trip = Sparbahn(start = start, target = target, fast = False, tripType='return', dateTo=datesTo[i], dateBack=datesBack[i])
     trip.getData()
-    trip.writeToFile()
+    trip.writeToFile("testdata")
+    tripdata_to = TripData()
+    tripdata_to.readTrip(trip.toData)
+    trips_to.append(tripdata_to)
+    
+    tripdata_back = TripData()
+    tripdata_back.readTrip(trip.backData)    
+    trips_back.append(tripdata_back)
 
-import json
-#trips = trip.toData
-with open('from_Dresden Hbf_to_N_at_16.02.2018_2018-02-15 17:59:29.501384.json','r') as datafile:
-    trips = json.loads(datafile.read())
-tripdata = TripData()
-tripdata.readTrip(trips)
-
-filteredtrips = tripdata.findFilter(dateTo,arrival_time="23:59",max_price = 100)
-
-
-print(filteredtrips)
-
-
-
+print(trips_to)
 
 
